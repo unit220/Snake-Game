5799e5d2e590e6a3dc82f0b27c66e883bb988db5
Bennett Marra <bmarra@email.sc.edu>
2021-09-27 18:03:13 -0400

    Add snake growth

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,11 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
-    void Start()
-    {
-        
+    private List<Transform> _segments;
+    public Transform segmentPrefab;
+    void Start() {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
     }
 
     private void Update()
@@ -24,11 +27,32 @@ public class Snake : MonoBehaviour
     // Called at a fixed timed interval (NOT frame based)
     private void FixedUpdate()
     {
+        /*
+        Moves the body of the snake
+        Starts at the tail and follows the segment in front of it
+        */
+        for(int i=_segments.Count-1; i>0; i--) {
+            _segments[i].position = _segments[i-1].position;
+        }
+
         this.transform.position = new Vector3(
             // Values are rounded to hold our snake to a grid
             Mathf.Round(this.transform.position.x) + _direction.x,
             Mathf.Round(this.transform.position.y) + _direction.y,
             0.0f
         );
+    }
+
+    private void Grow() {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count-1].position;
+
+        _segments.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Food") {
+        Grow();
+        }
     }
 }
