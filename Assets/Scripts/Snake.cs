35733223a6745efb526081cdb98ad8243e347d49
Bennett Marra <bmarra@email.sc.edu>
2021-09-27 14:59:50 -0400

    Add movement to the snake

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2 _direction = Vector2.right;
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W)) {
+            _direction = Vector2.up;
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            _direction = Vector2.down;
+        } else if (Input.GetKeyDown(KeyCode.A)) {
+            _direction = Vector2.left;
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            _direction = Vector2.right;
+        }
+    }
+
+    // Called at a fixed timed interval (NOT frame based)
+    private void FixedUpdate()
+    {
+        this.transform.position = new Vector3(
+            // Values are rounded to hold our snake to a grid
+            Mathf.Round(this.transform.position.x) + _direction.x,
+            Mathf.Round(this.transform.position.y) + _direction.y,
+            0.0f
+        );
+    }
+}
